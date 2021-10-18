339c47ce6ed1649b024da9ca503e46a105e6344d
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-10-18 19:30:38 +1300

    got some arrays

--- scrape.ts
+++ scrape.ts
@@ -1,13 +1,30 @@
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
+import cheerio from "cheerio";
 
 async function fetchLois() {
-    const res = await fetch("https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-health-advice-public/contact-tracing-covid-19/covid-19-contact-tracing-locations-interest")
-    const data = await res.text()
-    console.log('data',data)
+  const res = await fetch(
+    "https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-health-advice-public/contact-tracing-covid-19/covid-19-contact-tracing-locations-interest"
+  );
+  const body = await res.text();
+  const $ = cheerio.load(body);
+  const table = $(".view-content .views-table");
+
+  const trs = $(table).find("tr");
+  trs.each((i, tr) => {
+    const tds = $(tr).find("td");
+    const texts: string[] = [];
+    tds.each((i, td) => {
+      texts.push($(td).text().trim());
+      // console.log('tdText',)
+    });
+    console.log(texts);
+  });
+
+  //   console.log("body", body);
 }
 
 async function main() {
-    fetchLois()
+  fetchLois();
 }
 
-main()
+main();
