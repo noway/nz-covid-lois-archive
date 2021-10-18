20044d2daee5a0c6fa8be6faaa94e05f6c09663c
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-10-18 19:42:04 +1300

    some json objects

--- scrape.ts
+++ scrape.ts
@@ -1,6 +1,13 @@
 import fetch from "node-fetch";
 import cheerio from "cheerio";
-
+interface LOI {
+  'Location name': string
+  'Address': string
+  // 'Day': string
+  // 'Times': string
+  'What to do': string
+  'Updated': string
+}
 async function fetchLois() {
   const res = await fetch(
     "https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-health-advice-public/contact-tracing-covid-19/covid-19-contact-tracing-locations-interest"
@@ -11,25 +18,28 @@ async function fetchLois() {
 
   const ths = $(table).find("thead th");
 
-  const headings: string[] = [];
+  const headings: (keyof LOI)[] = [];
   ths.each((i, th) => {
-    const text = $(th).text().trim();
+    const text = $(th).text().trim() as keyof LOI;
     headings.push(text);
   });
   console.log(headings);
 
   const trs = $(table).find("tbody tr");
+  const data: Map<string, string> = new Map()
   trs.each((i, tr) => {
     const tds = $(tr).find("td");
     const texts: string[] = [];
+    const loi: Partial<Record<keyof LOI, string>> = {}
     tds.each((i, td) => {
-      texts.push($(td).text().trim());
-      // console.log('tdText',)
+      const heading = headings[i]
+      const text = $(td).text().trim()
+      loi[heading] = text
+      
     });
-    // console.log(texts);
+    console.log(loi)
   });
 
-  //   console.log("body", body);
 }
 
 async function main() {
